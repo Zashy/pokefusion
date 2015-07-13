5c74a4f07d1cdfa684e563bb345c3987506be79b
Zashy <greendarkness@gmail.com>
2015-07-13 16:56:34 -0500

    Minor updates.

--- app/scripts/modules/fusion.js
+++ app/scripts/modules/fusion.js
@@ -7,3 +7,18 @@ var pokemon1 = 1,
 	pokemon2 = 2;
 
 var randomOrgAPI = keyUtil.getRandomOrgAPIKey();
+
+function processRandomResult(){
+}
+
+var xhr = new XMLHttpRequest(),
+	data = {
+		"apiKey": '',
+		"n":      10,
+		"min":    1,
+		"max":   721
+	};
+xhr.onreadystatechange = processRandomResults;
+
+httpRequest.open('POST', 'https://api.random.org/json-rpc/1/invoke', true);
+httpRequest.send(data);
